46d05134a2b8435b668da0f1451abf8f5dc024ed
das <ardynn.d@gmail.com>
2019-09-13 10:52:03 +0700

    design music box

--- js/component/AllMusic.vue
+++ js/component/AllMusic.vue
@@ -1,52 +1,53 @@
 <template>
-  <div>
-      <h1>All Music</h1>
-      <div v-for="music in allMusic" :key="music._id">
+  <div class="d-flex flex-column justify-content-center">
+    <h1 class="text-center mt-5">All Music</h1>
+    <div class="row d-flex justify-content-center align-items-start mt-5">
+      <div v-for="music in allMusic" :key="music._id" class="shadow-lg m-2 col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6">
         <Audio :file="music.url"></Audio>
       </div>
+    </div>
   </div>
 </template>
 
 <script>
-import server from '../apis/server';
-import Audio from '../component/Audio'
-const { axios, serverURL , Swal } = server
+import server from "../apis/server";
+import Audio from "../component/Audio";
+const { axios, serverURL, Swal } = server;
 
 export default {
-    data : function () {
-        return  {
-            allMusic : []
+  data: function() {
+    return {
+      allMusic: []
+    };
+  },
+  methods: {
+    fetchAllMusic() {
+      const token = localStorage.getItem("token");
+      axios({
+        method: "GET",
+        url: `${serverURL}/music`,
+        headers: {
+          token
         }
-    },
-    methods : {
-        fetchAllMusic () {
-            const token = localStorage.getItem('token')
-            axios({
-                method : "GET",
-                url : `${serverURL}/music`,
-                headers : {
-                    token
-                }
-            })
-            .then( response => {
-                this.allMusic = response.data;
-                console.log( this.allMusic )
-            })
-            .catch( err => {
-                console.log("ERR")
-                console.log( err );
-            }) 
-        }
-    },
-    created : function (){
-        this.fetchAllMusic()
-    },
-    components : {
-        Audio
+      })
+        .then(response => {
+          this.allMusic = response.data;
+          console.log(this.allMusic);
+        })
+        .catch(err => {
+          console.log("ERR");
+          console.log(err);
+        });
     }
-}
+  },
+  created: function() {
+    this.fetchAllMusic();
+  },
+  components: {
+    Audio
+  }
+};
 </script>
 
 <style>
-
 </style>
